ba7e37b3d7e2523d0bef0b0bfae71b8b368554d1
Kutman <muctep.k@gmail.com>
2019-12-09 19:56:52 +0600

    Добавил модель Цитата

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = [
+    ('new', 'Новая'),
+    ('verified', 'Проверен')
+]
+STATUS_DEFAULT_CHOICE = 'new'
+
+
+class Quote(models.Model):
+    text = models.TextField(verbose_name='Текст цитаты')
+    author = models.CharField(max_length=128, verbose_name='Автор')
+    email = models.EmailField(verbose_name='Почта автора')
+    status = models.CharField(max_length=128, verbose_name='Статус', choices=STATUS_CHOICES, default=STATUS_DEFAULT_CHOICE, blank=True)
+    rating = models.IntegerField(verbose_name='Рейтинг', default=0, blank=True)
+    created_at = models.DateField(verbose_name='Дата создания', auto_now_add=True, blank=True)
+
