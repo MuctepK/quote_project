237af6719feb163e1691b5117e3a2f536cfee1ef
Kutman <muctep.k@gmail.com>
2019-12-09 20:04:24 +0600

    Добавлены пути, вход и выход через API

--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class LogoutView(APIView):
+    permission_classes = []
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        if user.is_authenticated:
+            user.auth_token.delete()
+        return Response({'status': 'ok'})
+
--- source/main/settings.py
+++ source/main/settings.py
@@ -101,7 +101,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
